9f953bb3ffd7aae0db2a5e80288975dd4c414502
Justin <justin@adalane.com>
2026-03-26 18:15:50 -0500

    fix: Rob feedback round 1 — copy changes #2 #4 #7 #10 #11 #12 + curated student reviews

---
+++ src/data/reviews-students-individual.js
@@ -0,0 +1,34 @@
+export default {
+  "reviews": [
+    {
+      "id": 1,
+      "author": "Verified User",
+      "rating": 5,
+      "text": "LIFE SAVER! As a young female in Manhattan, often working late nights, I was once violently attacked on the street walking home and have since then tried to be more cautious. The \"Track Me On The Go\" feature has been a lifesaver! Not only to my sense of well-being but also to my wallet. Fortunately, I haven't had to use any of the more aggressive features yet but it's definitely worth every penny."
+    },
+    {
+      "id": 2,
+      "author": "Verified User",
+      "rating": 5,
+      "text": "I often walk home alone late at night, usually after a long day. Most of the time it's fine, but there are def times when I feel nervous and even unsafe. Bond does just that for me. Easy to use — one click and you have someone there looking out for you, ready to call for help the second you need it. Highly recommend for anyone who ever walks alone, anywhere."
+    },
+    {
+      "id": 3,
+      "author": "Verified User",
+      "rating": 5,
+      "text": "This is the most impressive app I have ever used. As a female in and out of the city working late nights, it can be difficult to find something to make you feel safe. This app has completely changed my outlook on walking alone and being on public transportation at night. Knowing I can keep my phone in hand — have someone in contact with me, knowing my location, hearing my surroundings — lets you know you are in safe hands."
+    },
+    {
+      "id": 4,
+      "author": "Verified User",
+      "rating": 5,
+      "text": "It really gives me peace of mind when I'm leaving late by myself! Its super simple to use and very convenient! I recommend it to everyone!"
+    },
+    {
+      "id": 5,
+      "author": "Verified User",
+      "rating": 5,
+      "text": "Just recently downloaded the app and it makes me feel safe just knowing that I have it. I can call the app and it really helps me to know that I always have someone there on hand to help whenever need be."
+    }
+  ]
+};

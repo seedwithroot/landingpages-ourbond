db92ffdc7e9cbb49c5d1458ca87b3c2bf6a4ff8a
Justin <justin@adalane.com>
2026-03-26 13:20:55 -0500

    switch to docs/ output for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lp.ourbond.com',
+  site: 'https://seedwithroot.github.io',
+  base: '/landingpages-ourbond',
+  outDir: 'docs',
 });

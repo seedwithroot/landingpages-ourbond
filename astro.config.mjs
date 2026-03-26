8e1c8a22d9c2ac69a65c472148f117b33696cbd6
Justin <justin@adalane.com>
2026-03-26 18:24:40 -0500

    fix: set site to production URL, remove github pages base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seedwithroot.github.io',
-  base: '/landingpages-ourbond',
+  site: 'https://lp-ourbond.adalane.com',
   outDir: 'docs',
 });
